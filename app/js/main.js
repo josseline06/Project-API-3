cbaaeda04421e7305b75dea756c3c7e44929c6d7
Carlos Avilan <avilancj@gmail.com>
2015-04-29 02:07:12 -0430

    Versión más chévere del scrolling

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,20 @@
 /**
  * Main AngularJS Web Application
  */
-var app = angular.module('tutorialWebApp', ['ngRoute']);
+var app = angular.module('tutorialWebApp', ['ngRoute','duScroll']);
 
+app.controller('MyCtrl', function($scope, $document){
+    $scope.toTheTop = function() {
+      $document.scrollTopAnimated(0, 5000).then(function() {
+        console && console.log('You just scrolled to the top!');
+      });
+    }
+    var section3 = angular.element(document.getElementById('section-3'));
+    $scope.toSection3 = function() {
+      $document.scrollToElementAnimated(section3);
+    }
+  }
+).value('duScrollOffset', 30);
 /**
  * Configure the Routes
  */
